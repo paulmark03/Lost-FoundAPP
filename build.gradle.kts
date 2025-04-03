0e196c34574e057c24433ed19e5c0468a6209d62
Paul <paul.marcu2003@gmail.com>
2025-04-03 10:51:48 +0200

    removed git action

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.gms.google.services) apply false
-    id("org.sonarqube") version "6.0.1.5171"
 }
-sonar {
-    properties {
-        property("sonar.projectKey", "paulmark03_Lost-FoundAPP")
-        property("sonar.organization", "paulmark03")
-        property("sonar.host.url", "https://sonarcloud.io")
-    }
-}
+
